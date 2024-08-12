556cbf00d94cab51400dc47280be9fad004e9700
aliabedfayed <aliabedfayed@yahoo.com>
2024-08-12 19:57:00 +0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import path from 'path';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'about', component: AboutComponent },
+    { path: 'portfolio', component: PortfolioComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: "**", component: NotFoundComponent }
+];
